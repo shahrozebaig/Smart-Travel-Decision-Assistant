7b829353d9f9f37e41b87a0562fa0f3ff8a5f3c1
Shahroze Baig <shahrozeb98@gmail.com>
2026-04-20 22:33:56 +0530

    For Deploying

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 export async function analyzeCity(city) {
-  const res = await fetch(`http://127.0.0.1:8000/api/v1/weather/analyze?city=${city}`);
+  const res = await fetch(`https://smart-travel-decision-assistant.onrender.com/api/v1/weather/analyze?city=${city}`);
   if (!res.ok) {
     throw new Error("API Error");
   }
